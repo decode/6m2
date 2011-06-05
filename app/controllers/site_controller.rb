ddc89a9cc8af24e88a8061125e237acfb5f70a18
DecoDe <code723@gmail.com>
2011-06-05 19:12:25 +0800

    update

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -14,7 +14,8 @@ class SiteController < ApplicationController
     #@admins = User.where(:id => ids).order("created_at DESC")#.paginate(:page => params[:page], :per_page => 20)
 
     @users = User.where("username != 'superadmin'").order("created_at Desc").limit(5)
-    @articles = Article.order("created_at DESC").limit(10)
+    offs = rand(Article.count)
+    @articles = Article.order("created_at DESC").limit(10).offset(offs)
   end
 
   def setting
