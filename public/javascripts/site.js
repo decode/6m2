f2274bf0d9001117feeaa39306f927a819096a24
DecoDe <code723@gmail.com>
2011-07-06 21:13:22 +0800

    update css style

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -26,4 +26,36 @@ $(document).ready(function(){
 	});
 
   $("#data_table").tablesorter(); 
+
+  /*
+  var footerHeight = 0,
+      footerTop = 0,
+      $footer = $("#footer");
+
+  positionFooter();
+
+  function positionFooter() {
+
+    footerHeight = $footer.height();
+    footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
+
+    if ( ($(document.body).height()+footerHeight) < $(window).height()) {
+      $footer.css({
+        position: "absolute"
+      }).animate({
+        top: footerTop
+      })
+    } else {
+      $footer.css({
+        position: "static"
+      })
+    }
+
+  }
+
+  $(window)
+    .scroll(positionFooter)
+    .resize(positionFooter)
+  */
+
 });
