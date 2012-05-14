7906d33bd218581f1e1243410381613e6fda238e
DecoDe <code723@gmail.com>
2012-05-14 23:57:09 +0800

    update

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,8 @@ class Article < ActiveRecord::Base
 
   validates_uniqueness_of :title, :content
   validates_presence_of :title, :content
+
+  # Add after upgrade to rails 3.1
+  # 2012.5.14
+  attr_accessible :title, :content, :article_type
 end
